bc7182d0181870219b5e5b84a16e4fb84e84368b
Derrick Farris <mr.dcfarris@gmail.com>
2023-06-26 00:26:45 -0500

    Add server broadcasting user msgs

--- apps/bun-server/index.ts
+++ apps/bun-server/index.ts
@@ -1,22 +1,45 @@
-import { MsgDeserializer } from "@fast-chat-js/ws-serialization";
+import { MsgDeserializer, MsgSerializer } from "@fast-chat-js/ws-serialization";
 
+// TODO: Lazy load these?
 const deserializer = new MsgDeserializer();
-const { parseMsg } = deserializer;
+const serializer = new MsgSerializer();
 
-Bun.serve({
+const { parseMsg } = deserializer;
+const { serializeMsgObj } = serializer;
+
+Bun.serve<{ username: string }>({
   fetch(req, server) {
     // upgrade the request to a WebSocket
-    if (server.upgrade(req)) {
+    if (
+      server.upgrade(req, {
+        data: {
+          username: new URL(req.url).searchParams.get("username"),
+          // authToken: cookies["X-Token"],
+        },
+      })
+    ) {
       return; // do not return a Response
     }
     return new Response("Upgrade failed :(", { status: 500 });
   },
   websocket: {
     message(ws, message) {
-      const msg = parseMsg(message as Uint8Array);
+      // if msgType === 0 (userText)
+      // broadcast message to room
+      if (message[0] === 0) {
+        ws.publishBinary("main", message as Uint8Array);
+      }
     }, // a message is received
     open(ws) {
       ws.subscribe("main");
+      ws.publishBinary(
+        "main",
+        serializeMsgObj({
+          type: "systemUserAddedToRoom",
+          timestamp: Date.now(),
+          fields: { username: ws.data.username },
+        }),
+      );
       // ws.publish("main", stringifyMsg();
     }, // a socket is opened
     close(ws) {}, // a socket is closed
